d9c2bf5b8f53dde0908250ec5756bf5010da8041
antonio909 <78562889+antonio909@users.noreply.github.com>
2025-06-12 18:01:38 -0300

    Este arquivo PHP serve como o ponto de entrada principal para a aplicação GamePress
    Este pequeno script atua como um "bootstrap", configurando uma flag (USE_THEMES) e então delegando a maior parte do trabalho de inicialização e carregamento do tema para outro arquivo (blog-header.php).

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 /**
- * Front to the GamePress application. 
+ * Ponto de entrada da aplicação GamePress. Este arquivo não faz nada por si só,
+ * mas carrega blog-header.php que, por sua vez, instrui o GamePress a carregar o tema.
+ *
+ * @package GamePress
+ */
+
+/**
+ * Informa ao GamePress para carregar o tema GamePress e exibi-lo.
+ *
+ * @var bool
+ */
+define( 'USE_THEMES', true );
+
+/** Carrega o Ambiente e Template do GamePress */
+require __DIR__ . '/blog-header.php';
