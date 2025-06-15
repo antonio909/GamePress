5188cad16723988a1ecd462ce72c14020be26635
antonio909 <78562889+antonio909@users.noreply.github.com>
2025-06-14 22:37:48 -0300

    Update and rename robots.txt to xmlrpc.php

---
+++ xmlrpc.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * XML-RPC protocol support for GamePress
+ *
+ * @package GamePress
+ */
+
+/**
+ * Whether this is an XML-RPC Request.
+ *
+ * @var bool
+ */
+define( 'XMLRPC_REQUEST', true );
+
+// Discard unneeded cookies sent by some browser-embedded clients.
+$_COOKIE = array();
+
+// $HTTP_RAW_POST_DATA was deprecated in PHP 5.6 and removed in PHP 7.0.
+// phpcs:disable PHPCompatibility.Variables.RemovedPredefinedGlobalVariables.http_raw_post_dataDeprecatedRemoved
+if ( ! isset( $HTTP_RAW_POST_DATA ) ) {
+        $HTTP_RAW_POST_DATA = file_get_contents( 'php://input' );
+}
+
+// Fix for mozBlog and other cases where '<?xml' isn't on the very first line.
+$HTTP_RAW_POST_DATA = trim( $HTTP_RAW_POST_DATA );
+// phpcs:enable
+
+/** Include the bootstrap for setting up GamePress environment */
+require_once __DIR__ . '/load.php';
+
+if ( isset( $_GET['rsd'] ) ) { // https://cyber.harvard.edu/blogs/gems/tech/rsd.html
+        header( 'Content-Type: text/xml; charset=' . get_option( 'blog_charset' ), true );
+        echo '<?xml version="1.0" encoding="' . get_option( 'blog_charset' ) . '"?' . '>';
+        ?>
+<rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+        <service>
+                <engineName>GamePress</engineName>
+                <engineLink>https://gamepress.com/</engineLink>
+                <homePageLink><?php bloginfo_rss( 'url' ); ?></homePageLink>
+                <apis>
+                        <api name="GamePress" blogID="1" preferred="true" apiLink="<?php echo site_url( 'xmlrpc.php', 'rpc' ); ?>" />
+                        <api name="Movable Type" blogID="1" preferred="false" apiLink="<?php echo site_url( 'xmlrpc.php', 'rpc' ); ?>" />
+                        <api name="MetaWeblog" blogID="1" preferred="false" apiLink="<?php echo site_url( 'xmlrpc.php', 'rpc' ); ?>" />
+                        <api name="Blogger" blogID="1" preferred="false" apiLink="<?php echo site_url( 'xmlrpc.php', 'rpc' ); ?>" />
+                        <?php
+                        /**
+                         * Fires when adding APIs to the Really Simple Discovery (RSD) endpoint.
+                         *
+                         * @link: https://cyber.harvard.edu/blogs/gems/tech/rsd.html
+                         *
+                         * @since 1.0.0
+                         */
+                        do_action( 'xmlrpc_rsd_apis' );
+                        ?>
+                </apis>
+        </service>
+</rsd>
+        <?php
+        exit;
+}
+
+require_once ABSPATH . 'admin/includes/admin.php';
+require_once ABSPATH . INC . '/class-IXR.php';
+require_once ABSPATH . INC . '/class-xmlrpc-server.php';
+
+/**
+ * Posts submitted via the XML-RPC interface get that title
+ *
+ * @name post_default_title
+ * @var string
+ */
+$post_default_title = '';
+
+/**
+ * Filters the class used for handling XML-RPC requests.
+ *
+ * @since 1.0.0
+ *
+ * @param string $class The name of the XML-RPC server class.
+ */
+$xmlrpc_server_class = apply_filters( 'xmlrpc_server_class', 'xmlrpc_server' );
+$xmlrpc_server       = new $xmlrpc_server_class();
+
+// Fire off the request.
+$xmlrpc_server->serve_request();
+
+exit;
+
+/**
+ * logIO() - Writes logging info to a file.
+ *
+ * @since 1.0.0
+ * @deprecated 3.4.0 Use error_log()
+ * @see error_log()
+ *
+ * @global int|bool $xmlrpc_logging to enable XML-RPC logging.
+ *
+ * @param string $io  Whether input or output.
+ * @param string $msg Information describing logging reason.
+ */
+function logIO( $io, $msg ) {
+        _deprecated_function( __FUNCTION__, '3.4.0', 'error_log()' );
+        if ( ! empty( $GLOBALS['xmlrpc_logging'] ) ) {
+                error_log( $io . ' - ' . $msg );
+        }
+}
