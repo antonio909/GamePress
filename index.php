24849ed05823d56629ce69ace5115a34f143d98d
antonio909 <78562889+antonio909@users.noreply.github.com>
2025-06-15 12:43:25 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <?php
-/**
- * Ponto de entrada da aplicação GamePress. Este arquivo não faz nada por si só,
- * mas carrega blog-header.php que, por sua vez, instrui o GamePress a carregar o tema.
- *
- * @package GamePress
- */
 
 /**
- * Informa ao GamePress para carregar o tema GamePress e exibi-lo.
+ * @file
+ * A página PHP que atende a todas as requisições de página em uma instalação GamePress.
  *
- * @var bool
+ * Todo o código GamePress é lançado sob a Licença Pública Geral GNU.
+ * Veja os arquivos COPYRIGHT.txt e LICENSE.txt no diretório 'core'.
  */
-define( 'USE_THEMES', true );
 
-/** Carrega o Ambiente e Template do GamePress */
-require __DIR__ . '/blog-header.php';
+use GamePress\Core\GamePressKernel;
+use Symfony\Component\HttpFoundation\Request;
+
+$autoloader = require_once 'autoload.php';
+
+$kernel = new GamePressKernel('prod', $autoloader);
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+
+$kernel->terminate($request, $response);
